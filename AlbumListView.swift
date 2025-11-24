78d3d7f337c6ed086ab1c1226d4b5069a9513c5b
AbhishekChoudhary11 <131265140+AbhishekChoudhary11@users.noreply.github.com>
2025-11-25 00:19:33 +0530

    Add files via upload

---
+++ AlbumListView.swift
@@ -0,0 +1,40 @@
+//  AlbumListView.swift
+//  ItunesSearchApp
+//
+//  Created by Abhishek on 01.08.24.
+//
+
+import SwiftUI
+
+struct AlbumListView: View {
+    
+    @ObservedObject var viewModel: AlbumListViewModel
+    
+    var body: some View {
+        
+        List {
+            ForEach(viewModel.albums) { album in
+                NavigationLink {
+                    AlbumDetailView(album: album)
+                } label: {
+                    AlbumRowView(album: album)
+                }
+
+            }
+            
+            ListPlaceholderRowView(state: viewModel.state,
+                                   loadMore: viewModel.loadMore)
+        }
+        .listStyle(.plain)
+        
+    }
+}
+
+struct AlbumListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AlbumListView(viewModel: AlbumListViewModel.example())
+        }
+       
+    }
+}
