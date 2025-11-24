67a30d22e8e5de2feca7d61da4c0b3cd4d3501c5
AbhishekChoudhary11 <131265140+AbhishekChoudhary11@users.noreply.github.com>
2025-11-25 00:17:08 +0530

    Add files via upload

---
+++ ItunesSearchAppApp.swift
@@ -0,0 +1,16 @@
+//  ItunesSearchAppApp.swift
+//  ItunesSearchApp
+//
+//  Created by Abhishek on 25.07.24.
+//
+
+import SwiftUI
+
+@main
+struct ItunesSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
