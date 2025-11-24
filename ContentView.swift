558c6719ab587ec4f23404c567ed50c9e87477c0
AbhishekChoudhary11 <131265140+AbhishekChoudhary11@users.noreply.github.com>
2025-11-25 00:16:20 +0530

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,37 @@
+//  ContentView.swift
+//  ItunesSearchApp
+//
+//  Created by Abhishek on 25.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            
+            SearchView()
+                .tabItem {
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+            
+            AlbumSearchView()
+                .tabItem {
+                    Label("Albums", systemImage: "music.note")
+                }
+            
+            MovieSearchListView()
+                .tabItem {
+                    Label("Movies", systemImage: "tv")
+                }
+        }
+          
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
